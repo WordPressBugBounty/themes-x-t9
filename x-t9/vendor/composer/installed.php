e8803e81893fc3777b234cd677f04835e8e8235c
Sean Murphy <sean@iamseanmurphy.com>
2024-10-14 01:07:25 +0000

    1.26.4

---
+++ x-t9/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '3def443dee7c33d9ea54c16b4f906286ca08b19c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '3def443dee7c33d9ea54c16b4f906286ca08b19c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'vektor-inc/tgm-plugin-activation' => array(
+            'pretty_version' => '2.6.2',
+            'version' => '2.6.2.0',
+            'reference' => 'ae20a712c7bbd67f1bef0af9a3f5f07359a94d48',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../vektor-inc/tgm-plugin-activation',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
