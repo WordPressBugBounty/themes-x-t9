8b12e1b08ea2b9ce7794642ea1fca6c099b9d12f
Sean Murphy <sean@iamseanmurphy.com>
2026-03-24 19:34:10 +0000

    1.37.4

--- x-t9/vendor/composer/autoload_files.php
+++ x-t9/vendor/composer/autoload_files.php
@@ -6,5 +6,8 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    '6124b4c8570aa390c21fafd04a26c69f' => $vendorDir . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
+    'ec07570ca5a812141189b1fa81503674' => $vendorDir . '/phpunit/phpunit/src/Framework/Assert/Functions.php',
     '23cc7d20cf0e5cb79054a1631f7842e6' => $vendorDir . '/vektor-inc/tgm-plugin-activation/class-tgm-plugin-activation.php',
+    '7d3b315c4f303f2fc14aca642a738e50' => $vendorDir . '/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php',
 );
