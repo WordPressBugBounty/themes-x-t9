2ec2deea9dbe4d06ee0df9b417a78a0c376f7e9e
Sean Murphy <sean@iamseanmurphy.com>
2026-05-27 09:29:21 +0000

    1.39.2

--- x-t9/vendor/composer/installed.php
+++ x-t9/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.39.1',
-        'version' => '1.39.1.0',
-        'reference' => 'abcc44163f3a09a05bd6e2be51f3778699fa6207',
+        'pretty_version' => '1.39.2',
+        'version' => '1.39.2.0',
+        'reference' => '7ca0a4e31b5e565fde30cb60e67c9b34ca9d0f5a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.39.1',
-            'version' => '1.39.1.0',
-            'reference' => 'abcc44163f3a09a05bd6e2be51f3778699fa6207',
+            'pretty_version' => '1.39.2',
+            'version' => '1.39.2.0',
+            'reference' => '7ca0a4e31b5e565fde30cb60e67c9b34ca9d0f5a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
